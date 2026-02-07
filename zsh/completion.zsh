6bfe2f425e88b912a3db6391f76b05ae69522bff
Anirudh Sriram <techwithanirudh@gmail.com>
2026-02-07 03:28:26 +0000

    feat: add terminal keybindings configuration and enhance Zsh completion script

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,5 +1,11 @@
+#!/usr/bin/env zsh
+
 # matches case insensitive for lowercase
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+# Use menu selection instead of prompting to list many options.
+zstyle ':completion:*' menu select
+setopt AUTO_MENU
