246e1454595a9292aa709c0ad1741d552c63f895
Anirudh <70373803+techwithanirudh@users.noreply.github.com>
2026-02-07 05:58:54 +0000

    feat: ai

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -2,5 +2,8 @@
 export LANG="en_US.UTF-8"
 export LC_ALL="en_US.UTF-8"
 
+# Set default timezone for CLI tools that respect TZ
+export TZ="Asia/Kolkata"
+
 # Don't clear the screen after quitting a manual page
 export MANPAGER="less -X"
